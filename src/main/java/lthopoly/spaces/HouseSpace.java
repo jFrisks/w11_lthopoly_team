8fd0305553066509dd87f4ed7be0b622e2931663
Mattias Svensson <33493010+mattias-92@users.noreply.github.com>
2017-11-23 12:33:17 +0100

    Update HouseSpace.java

--- src/main/java/lthopoly/spaces/HouseSpace.java
+++ src/main/java/lthopoly/spaces/HouseSpace.java
@@ -2,15 +2,26 @@ package lthopoly.spaces;
 
 
 import lthopoly.GameBoard;
+import lthopoly.Player;
 
-/**
- * Created by Tank on 4/17/2016.
- */
+import java.util.ArrayList;
+import java.lang.Integer;
+
 public class HouseSpace extends BoardSpace {
+    int rent;
+    String description;
+    Boolean owned;
+    Player owner;
+    int[] houseActions;
+
+
     /**
      * Creates a new HouseSpace with rent and a description
      */
     public HouseSpace(int rent, String description) {
+        this.rent = rent;
+        this.description = description;
+        owned = false;
     }
 
     /**
@@ -18,7 +29,12 @@ public class HouseSpace extends BoardSpace {
      */
     @Override
     public int[] getPossibleActions(GameBoard board) {
-        return null;
+        if(owned){
+            houseActions = new int[]{3, 5, 6, 7};
+        }else{
+            houseActions = new int[]{2, 4, 5, 6, 7};
+        }
+        return houseActions;
     }
 
     /**
@@ -26,6 +42,16 @@ public class HouseSpace extends BoardSpace {
      */
     @Override
     public void action(GameBoard board, int action) {
+        if(action == 2){
+            board.getCurrentPlayer().adjustMoney(-rent);
+            owner = board.getCurrentPlayer();
+            owned = true;
+        }else if(action == 3){
+            board.getCurrentPlayer().adjustMoney(-rent);
+            owner.adjustMoney(rent);
+        }else {
+            board.doAction(action);
+        }
     }
 
     /**
@@ -33,7 +59,7 @@ public class HouseSpace extends BoardSpace {
      */
     @Override
     public String toString() {
-        return null;
+        return String.format("%s [%2s] (%3s)",description, owner, rent);
     }
 
 }
