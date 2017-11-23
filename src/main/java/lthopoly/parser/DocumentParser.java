3b1fa271737d36bc69dc3a80f9f4d53ff817e4c1
Jonathan Frisk <jontesport@gmail.com>
2017-11-23 08:55:19 +0100

    automatic idea shit

--- src/main/java/lthopoly/parser/DocumentParser.java
+++ src/main/java/lthopoly/parser/DocumentParser.java
@@ -4,7 +4,12 @@ import lthopoly.cards.MoneyCard;
 import lthopoly.cards.MoveCard;
 import lthopoly.spaces.BoardSpace;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Tank on 4/17/2016.
@@ -15,6 +20,24 @@ public class DocumentParser {
      * Returns an ArrayList of BoardSpaces loaded from a file
      */
     public static ArrayList<BoardSpace> getBoard() {
+        List<BoardSpace> board = new ArrayList<BoardSpace>();
+        BufferedReader br = null;
+        try{
+            br = new BufferedReader(new FileReader("../../board.txt"));
+            String line;
+            while((line = br.readLine()) != null){
+                //TODO:TOLKA LINE
+                //TODO:LÄGG IN I Board-arraylist
+
+                board.add(line)
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
